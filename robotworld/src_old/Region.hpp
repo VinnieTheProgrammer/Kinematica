efa45736aea2f5ddc565f016be9b24b3a6c82cf5
vincent <va.nienhuis@gmail.com>
2024-03-25 15:48:06 +0100

    removed unwanted files

--- robotworld/src_old/Region.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef REGION_HPP_
-#define REGION_HPP_
-
-#include "Config.hpp"
-
-#include <wx/region.h>
-
-#endif // SIZE_HPP_
--- robotworld/src_old/Size.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SIZE_HPP_
-#define SIZE_HPP_
-
-#include "Config.hpp"
-
-#include <wx/gdicmn.h>
-
-#endif // SIZE_HPP_
